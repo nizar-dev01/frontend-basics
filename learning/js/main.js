03f7d1a35f8a04e103ac0aa2acc19966babd6d9d
niya <niyafathimak1@gmail.com>
2022-05-14 20:29:16 +0530

    Changed file names

--- learning/js/main.js
+++ learning/js/main.js
@@ -1,1 +1,35 @@
-console.log("Hello there!")
+var count = 1;
+var end = 300;
+var interval = 1;
+
+var counter = document.getElementById("counter");
+var intervalID;
+
+var colors = [
+    'red',
+    'green',
+    'blue',
+    'orange',
+    'pink',
+    'purple',
+    'black',
+    'violet'
+];
+
+var colorCount = colors.length;
+
+function increment() {
+    if (count >= end) {
+        clearInterval(intervalID);
+    }
+    counter.innerHTML = count;
+    var clIndex = count % colorCount;
+    if(clIndex == colors.length - 1){
+        console.count("Iteration has been completed.");
+    }
+    var color = colors[clIndex];
+    counter.style.color = color;
+    count++
+}
+
+intervalID = setInterval(increment, interval);
